a6f5cdc8682da14eac458ead2bac9f7a0ee0142a
Jose Calderon <Jose Calderon@host.docker.internal>
2023-02-10 15:47:31 -0500

    first_commit

---
+++ culqi.net/culqinet/model/Security.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace culqi.net
+{
+	public class Security
+	{	
+		public Security()
+		{
+		}
+
+		public string public_key { get; set; }
+		public string secret_key { get; set; }
+
+	}
+}
---
+++ culqi.net/culqinet/test/EmptyClass.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace culqinet.test
+{
+    [TestClass]
+    public class EmptyClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Equals("A", "A");
+        }
+    }
+}
