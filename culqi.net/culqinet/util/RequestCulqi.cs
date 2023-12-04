6847b812c6cef8e19ee9a8537e8879dec833fda1
Tekton <lorenaortizdezevallos@MacBook-Pro-de-Lorena.local>
2023-12-04 03:23:27 -0500

    feature: params validation

--- culqi.net/culqinet/util/RequestCulqi.cs
+++ culqi.net/culqinet/util/RequestCulqi.cs
@@ -84,10 +84,16 @@ namespace culqi.net
                     request.AddJsonBody(model);
                 }
             }
+
             //string output = JsonConvert.SerializeObject(model);
             //Console.WriteLine(output);
-            request.AddHeader("Content-Type", "application/json");
+            request.Add("Content-Type", "application/json");
             request.AddHeader("Authorization", "Bearer " + api_key);
+            request.AddHeader("x-culqi-env", config.x_culqi_env);
+            request.AddHeader("x-api-version", config.x_api_version);
+            request.AddHeader("x-culqi-client", config.x_culqi_client);
+            request.AddHeader("x-culqi-client-version", config.x_culqi_client_version);
+
             if (rsa_id != null)
             {
                 request.AddHeader("x-culqi-rsa-id", rsa_id);
