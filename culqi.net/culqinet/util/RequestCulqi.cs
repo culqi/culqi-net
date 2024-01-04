db4df697d2a23254818aa763ade76503ba3afc89
Tekton <lorenaortizdezevallos@MacBook-Pro-de-Lorena.local>
2024-01-04 11:21:14 -0500

    fix: headers

--- culqi.net/culqinet/util/RequestCulqi.cs
+++ culqi.net/culqinet/util/RequestCulqi.cs
@@ -85,11 +85,17 @@ namespace culqi.net
                 }
             }
 
+            var env = config.x_culqi_env_live;
+
+            if(api_key.Contains("test")) {
+                env = config.x_culqi_env_test;
+            }
+
             //string output = JsonConvert.SerializeObject(model);
             //Console.WriteLine(output);
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("Authorization", "Bearer " + api_key);
-            request.AddHeader("x-culqi-env", config.x_culqi_env);
+            request.AddHeader("x-culqi-env", env);
             request.AddHeader("x-api-version", config.x_api_version);
             request.AddHeader("x-culqi-client", config.x_culqi_client);
             request.AddHeader("x-culqi-client-version", config.x_culqi_client_version);
@@ -101,6 +107,7 @@ namespace culqi.net
             RestResponse response = client.Execute(request);
             /*respCulqi.statusCode = (int) response.StatusCode;
             respCulqi.body = response.Content;*/
+            Console.WriteLine(request);
             Console.WriteLine(response.Content);
             return response;
 
